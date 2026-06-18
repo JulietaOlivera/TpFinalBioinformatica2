36dcef6a0954cd522fe43676626f3e27d554848c
JulietaOlivera <julietaoliveravilla@gmail.com>
2026-06-18 12:27:40 -0300

    Update restriccion.py
    agrego docstring

--- primers/restriccion.py
+++ primers/restriccion.py
@@ -28,10 +28,10 @@ def analizar_amplicones(resultados_primers):
     Recorre los amplicones y cuenta los sitios de cada enzima.
     """
     filas = []
-    for resultado in resultados_primers:
+    for resultado in resultados_primers: #en cada iteracion resultado es un diccionario correspondiente a un amplicon
         nombre    = resultado["nombre"]
         secuencia = resultado["seq_amplicón"]
-        for enzima, patron in ENZIMAS.items():
+        for enzima, patron in ENZIMAS.items(): #para cada amplicón se revisan todas las enzimas (items devuelve por ejemplo ("EcoRI", "GAATTC"))
             cantidad = contar_sitios(secuencia, patron)
             filas.append({
                 "amplicón": f"Amplicón_{nombre}",
